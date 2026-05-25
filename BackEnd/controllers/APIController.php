10fba419bf900e8d0026d45b846ad68f1ebe5a82
JustFr4nck <francyperry07@gmail.com>
2026-05-25 18:52:59 +0200

    backend setup for db and first service added

--- BackEnd/controllers/APIController.php
+++ BackEnd/controllers/APIController.php
@@ -1,9 +1,33 @@
 <?php
+
+use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-
 class APIController
 {
-    
+    protected $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    public function index(Request $request, Response $response, array $args)
+    {
+        // Collegamento al db e alla collezione book
+        $db = $this->container->get('db');
+        $collection = $db->selectCollection('book');
+
+        // Salvare tutti i documenti in un cursor
+        $cursor = $collection->find([]);
+
+        // Trasformare il cursor in un array 
+        $libri = iterator_to_array($cursor);
+
+
+        // Response
+        $response->getBody()->write(json_encode($libri));
+        return $response->withHeader('Content-Type', 'application/json')->withStatus(200);
+    }
 }
--- BackEnd/index.php
+++ BackEnd/index.php
@@ -1,17 +1,36 @@
 <?php
-error_reporting(0);
+ini_set('display_errors', 1);
+error_reporting(E_ALL);
+
 header('Content-Type: application/json');
+
+use Psr\Container\ContainerInterface;
 use Slim\Factory\AppFactory;
+use MongoDB\Client;
 
-use Psr\Http\Message\ResponseInterface as Response;
-use Psr\Http\Message\ServerRequestInterface as Request;
-
+require __DIR__ . '/vendor/autoload.php';
 require __DIR__ . '/controllers/APIController.php';
 
+// 1. Inizializzazione container
+$container = new \DI\Container();
+
+// 2. Registra la connessione MongoDB
+$container->set('db', function (ContainerInterface $container) {
+    $uri = "mongodb://localhost:27017"; 
+    $client = new Client($uri);
+    return $client->selectDatabase('bookShop');
+});
+
+// 3. Creazione app
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
+// Rileva dinamicamente il percorso corretto dell'index.php eliminando i conflitti di routing
+$basePath = str_replace('/index.php', '', $_SERVER['SCRIPT_NAME']);
+$app->setBasePath($basePath);
+$app->addRoutingMiddleware();
+$app->addErrorMiddleware(true, true, true);
 
-$app->get('/api/booklist', "APIController:index");
-
+$app->get('/list', 'APIController:index');
 
 $app->run();
